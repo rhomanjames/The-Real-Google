a34c09bebb6527cfd0918ddefac29359ac9ce4ac
Rhoman James <rhomanmjames@gmail.com>
2021-08-08 03:54:34 -0400

    config files edited

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ module.exports = {
     domains: ["www.jobalign.com", "boutiquephotographer.com"],
   },
   reactStrictMode: true,
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+        // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+        config.resolve.fallback = {
+            fs: false
+        }
+    }
+
+    return config;}
 }
